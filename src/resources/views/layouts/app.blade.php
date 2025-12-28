652f38caa5874ec4b311cb7017dd51fece81aa7f
suzuka <mariguanglai@gmail.com>
2025-12-28 18:19:05 +0900

    20251228/2

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attendance Management</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+
+    <style>
+        /* ▼ 追加：ログアウトフォームを横並び用に調整 */
+        .header-nav__form {
+            display: inline;
+            /* ボタンと同じ高さで横並びにする */
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+
+                <a class="header__logo" href="/">
+                    <img src="{{ asset('images/coachtech.png') }}" alt="ロゴ">
+                </a>
+
+                <form action="{{ route('products.index') }}" method="GET" class="header-search">
+                    <input
+                        type="text"
+                        name="keyword"
+                        class="header-search-input"
+                        placeholder="何をお探しですか？"
+                        value="{{ request('keyword') }}">
+                </form>
+
+                <nav>
+                    <ul class="header-nav">
+                        @if (Auth::check())
+
+                        <!-- ログアウト -->
+                        <li class="header-nav__item">
+                            <form action="/logout" method="post" class="header-nav__form">
+                                @csrf
+                                <button class="header-nav__button">ログアウト</button>
+                            </form>
+                        </li>
+
+                        <!-- マイページ -->
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/mypage/profile">マイページ</a>
+                        </li>
+
+                        <!-- 出品 -->
+                        <li class="header-nav__item">
+                            <a class="header-nav__link header-nav__link--sell" href="/sell">出品</a>
+
+                        </li>
+
+
+
+                        @endif
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TestController::class, 'index']);
+Route::middleware('auth')->group(function () {
+     Route::get('/', [AuthController::class, 'index']);
+ });
