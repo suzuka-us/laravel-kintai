6ff01a794f223ea29068f73245163b06ba9df9ed
suzuka <mariguanglai@gmail.com>
2026-01-10 20:51:17 +0900

    20260110

--- src/database/seeders/BreakTimeSeeder.php
+++ src/database/seeders/BreakTimeSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\BreakTime;
+use Carbon\Carbon;
 
 class BreakTimeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        BreakTime::create([
+            'attendance_id' => 1, // attendances.id
+            'break_start'   => Carbon::today()->setTime(12, 0),
+            'break_end'     => Carbon::today()->setTime(13, 0),
+        ]);
     }
 }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AttendanceSeeder::class,
+            BreakTimeSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
