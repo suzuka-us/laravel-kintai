dc8be3cffb5b7cd28926f6a69f3b5099e95cf729
suzuka <mariguanglai@gmail.com>
2026-01-10 19:35:45 +0900

    20260110

--- src/database/seeders/AttendanceSeeder.php
+++ src/database/seeders/AttendanceSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Attendance;
+use Carbon\Carbon;
 
 class AttendanceSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Attendance::create([
+            'user_id'   => 1, // usersテーブルに存在するID
+            'work_date' => Carbon::today(),
+            'clock_in'  => null,
+            'clock_out' => null,
+            'status'    => '勤務外',
+            'remark'    => null,
+        ]);
     }
 }
